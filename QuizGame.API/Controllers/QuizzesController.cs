145b55a1fb577e9f6d65617beb811ca8c35bf2cd
Sebastian <mrseb10@gmail.com>
2024-05-18 17:14:13 +0200

    QuizzesController missing file

---
+++ QuizGame.API/Controllers/QuizzesController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using QuizGame.Data.Services;
+using QuizGame.Data.Services.DTO.QuizDTOs;
+
+namespace QuizGame.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class QuizzesController(IQuizzesService quizzesService) : ControllerBase
+{
+    private readonly IQuizzesService _quizzesService = quizzesService;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<QuizResponse>>> GetQuizzes()
+    {
+        IEnumerable<QuizResponse> quizzes = await _quizzesService.GetQuizzesAsync();
+        return Ok(quizzes);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddQuiz(QuizRequest quizRequest)
+    {
+        await _quizzesService.AddQuizAsync(quizRequest);
+        return CreatedAtAction(nameof(AddQuiz), quizRequest);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateQuiz(int id, QuizRequest quizRequest)
+    {
+        if (id != quizRequest.QuizId) return NotFound();
+        await _quizzesService.UpdateQuizAsync(quizRequest);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteQuiz(int id)
+    {
+        await _quizzesService.DeleteQuizAsync(id);
+        return NoContent();
+    }
+}
