39f84db4aeaa663ce222d19fd6ab37e36223dfee
Sebastian <mrseb10@gmail.com>
2024-05-19 11:07:56 +0200

    Main Menu

--- QuizGame.UI/src/app/app.routes.ts
+++ QuizGame.UI/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {MainMenuComponent} from "../components/main-menu/main-menu.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MainMenuComponent},
+];
