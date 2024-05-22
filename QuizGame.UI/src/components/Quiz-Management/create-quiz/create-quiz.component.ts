e32a02c12d5eab8a9112f15fc2bc3bac8aa37a5a
Sebastian <mrseb10@gmail.com>
2024-05-22 11:02:22 +0200

    Quiz Creator first step completed - Input Validation and styling

--- QuizGame.UI/src/components/Quiz-Management/create-quiz/create-quiz.component.ts
+++ QuizGame.UI/src/components/Quiz-Management/create-quiz/create-quiz.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
-import {BackButtonComponent} from "../../shared/back-button/back-button.component";
+import { BackButtonComponent } from '../../shared/back-button/back-button.component';
+import { NgClass } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-quiz',
   standalone: true,
-  imports: [BackButtonComponent],
+  imports: [
+    BackButtonComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    NgClass,
+    RouterLink,
+  ],
   templateUrl: './create-quiz.component.html',
   styleUrl: './create-quiz.component.css',
 })
-export class CreateQuizComponent {}
+export class CreateQuizComponent {
+  quizInfoFormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    questionsAmount: new FormControl(5, [
+      Validators.required,
+      Validators.min(5),
+      Validators.max(15),
+    ]),
+  });
+
+  proceed() {
+    console.log(this.quizInfoFormGroup.value);
+    this.quizInfoFormGroup.markAllAsTouched();
+    if (this.quizInfoFormGroup.valid) {
+      console.log('Proceeding...');
+    }
+  }
+}
---
+++ QuizGame.UI/src/components/Quiz-Management/stepper/stepper.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-stepper',
+  standalone: true,
+  imports: [],
+  templateUrl: './stepper.component.html',
+  styleUrl: './stepper.component.css',
+})
+export class StepperComponent {
+  @Input() stepsAmount: number = 5;
+  @Input() quizName: string = '';
+  currentStep: number = 1;
+
+  next() {
+    if (this.currentStep === this.stepsAmount) {
+      return;
+    }
+    this.currentStep++;
+  }
+
+  previous() {
+    if (this.currentStep === 1) {
+      return;
+    }
+    this.currentStep--;
+  }
+}
