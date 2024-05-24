d5a9a21a28bbec1e96ea29f2b446dd0c3e07b139
Sebastian <mrseb10@gmail.com>
2024-05-24 11:26:19 +0200

    Shuffling Question and Answers using Fisher-Yates Algorithm

--- QuizGame.UI/src/components/Game-Session/game-session/game-session.component.ts
+++ QuizGame.UI/src/components/Game-Session/game-session/game-session.component.ts
@@ -4,11 +4,13 @@ import { QuizDetails } from '../../../models/QuizDetails';
 import { GameService } from '../../../services/game.service';
 import { QuizzesService } from '../../../services/quizzes.service';
 import { GameReqDTO } from '../../../models/GameReqDTO';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ErrorsService } from '../../../services/errors.service';
 import { AsyncPipe } from '@angular/common';
 import { LoadingSpinnerComponent } from '../../shared/loading-spinner/loading-spinner.component';
 import { ErrorComponent } from '../../shared/error/error.component';
+import { Router } from '@angular/router';
+import { Question } from '../../../models/Question';
 
 @Component({
   selector: 'app-game-session',
@@ -26,18 +28,47 @@ export class GameSessionComponent {
     private gameService: GameService,
     private quizzesService: QuizzesService,
     private errorsService: ErrorsService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
     this.gameService.newGame$.subscribe((game) => (this.game = game));
+    this.validateGameData();
     if (this.game) {
-      this.quiz$ = this.quizzesService.getQuiz(this.game.quizId!);
+      this.quiz$ = this.quizzesService.getQuiz(this.game.quizId!).pipe(
+        map((quiz: QuizDetails) => {
+          quiz.questions = this.shuffleQuestionsAndAnswers(quiz.questions);
+          return quiz;
+        }),
+      );
     }
   }
 
   retry() {
     if (this.game) {
       this.quizzesService.getQuiz(this.game.quizId!).subscribe();
+    }
+  }
+
+  private shuffleQuestionsAndAnswers(questionsArr: Question[]): Question[] {
+    const questions = this.shuffle(questionsArr);
+    questions.forEach((question) => {
+      this.shuffle(question.answers);
+    });
+    return questions;
+  }
+
+  private shuffle<T>(arr: T[]): T[] {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  private validateGameData() {
+    if (!this.game?.quizId) {
+      this.router.navigate(['play']);
     }
   }
 }
