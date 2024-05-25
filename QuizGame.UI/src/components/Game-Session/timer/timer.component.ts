38f3e26d9c5ed5f91f4b8a84a7c35b64b19e66d0
Sebastian <mrseb10@gmail.com>
2024-05-25 03:14:13 +0200

    Timer service, counting down timer, pausing on selecting answer and resetting on next question

--- QuizGame.UI/src/components/Game-Session/timer/timer.component.ts
+++ QuizGame.UI/src/components/Game-Session/timer/timer.component.ts
@@ -1,13 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, numberAttribute, OnChanges, OnInit } from "@angular/core";
 import { NgStyle } from '@angular/common';
+import { TimerService } from '../../../services/timer.service';
+import { Answer } from "../../../models/Answer";
 
 @Component({
   selector: 'countdown-timer',
   standalone: true,
   imports: [NgStyle],
   templateUrl: './timer.component.html',
-  styleUrl: 'timer.component.css',
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit, OnChanges{
   @Input() time: number | null = null;
+  timeLeft: number | null = null;
+
+  @Input() selectedAnswer : Answer | null = null;
+
+  constructor(private timerService: TimerService) {}
+
+  ngOnInit() {
+    this.start()
+
+    this.timerService.timeLeft$.subscribe(
+      (timeLeft) => (this.timeLeft = timeLeft),
+    );
+  }
+
+  ngOnChanges() {
+    if (this.selectedAnswer) {
+      this.stop()
+    } else {
+      this.start()
+    }
+  }
+
+  start() {
+    if (this.time !== null) {
+      this.timerService.start(this.time);
+    }
+  }
+
+  stop() {
+    this.timerService.stop();
+  }
 }
