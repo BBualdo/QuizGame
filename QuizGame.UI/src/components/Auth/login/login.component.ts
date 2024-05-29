3961df8386293ab923eaec60f74093150b8c614e
Sebastian <mrseb10@gmail.com>
2024-05-29 18:30:21 +0200

    Login/Signup components structure and styling

--- QuizGame.UI/src/components/Auth/login/login.component.ts
+++ QuizGame.UI/src/components/Auth/login/login.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { BackButtonComponent } from "../../shared/back-button/back-button.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [BackButtonComponent, RouterLink],
   templateUrl: './login.component.html',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
--- QuizGame.UI/src/components/Auth/sign-up/sign-up.component.ts
+++ QuizGame.UI/src/components/Auth/sign-up/sign-up.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { BackButtonComponent } from '../../shared/back-button/back-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [BackButtonComponent, RouterLink],
   templateUrl: './sign-up.component.html',
 })
-export class SignUpComponent {
-
-}
+export class SignUpComponent {}
