cf2fe8991d58cdaba6721ecff745698419124ef6
Sebastian <mrseb10@gmail.com>
2024-05-30 18:55:37 +0200

    Login form template

--- QuizGame.UI/src/components/Auth/login/login.component.ts
+++ QuizGame.UI/src/components/Auth/login/login.component.ts
@@ -1,11 +1,47 @@
-import { Component } from "@angular/core";
-import { BackButtonComponent } from "../../shared/back-button/back-button.component";
-import { RouterLink } from "@angular/router";
+import { Component } from '@angular/core';
+import { BackButtonComponent } from '../../shared/back-button/back-button.component';
+import { RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { passwordValidator } from '../../../validators/password.validator';
+import { UserLogin } from '../../../models/DTOs/UserLogin';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [BackButtonComponent, RouterLink],
+  imports: [BackButtonComponent, RouterLink, ReactiveFormsModule, NgClass],
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(40),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      passwordValidator,
+    ]),
+  });
+
+  submit() {
+    this.loginForm.markAllAsTouched();
+    const formValues = this.loginForm.value;
+
+    if (this.loginForm.valid) {
+      const user: UserLogin = {
+        username: formValues.username,
+        password: formValues.password,
+      };
+
+      console.log(user);
+    }
+  }
+}
