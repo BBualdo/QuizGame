d7340ccedeb1a5bf3d4c61dc2e78e9012e28be55
Sebastian <mrseb10@gmail.com>
2024-06-16 13:47:49 +0200

    Included environment.ts

---
+++ QuizGame.UI/src/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  google: {
+    clientId:
+      '388434380096-skpup85boq0ndkdon7ntpjlupj42gq6i.apps.googleusercontent.com',
+    redirectUri: 'http://localhost:4200/auth/signin-google',
+  },
+  facebook: {
+    appId: '483461190715590',
+    redirectUri: 'http://localhost:4200/auth/signin-facebook',
+  },
+  microsoft: {
+    clientId: '634fee07-96d8-4e62-819e-23d0f11c739c',
+    redirectUri: 'http://localhost:4200/auth/signin-microsoft',
+  },
+  github: {
+    clientId: 'Ov23lidVevGtNmWfQRjR',
+    redirectUri: 'http://localhost:4200/auth/signin-github',
+  },
+  twitter: {
+    clientId: 'aHFDZDhNcDBBaE93UmVyc1djeUs6MTpjaQ',
+    redirectUri: 'http://localhost:4200/auth/signin-twitter',
+  },
+};
